16756d0dfacad7ccda3cb9f3b0f6e59bd6530e08
Jason Lau <jason.lau@axiros.com>
2019-09-28 18:30:54 +0800

    added attribution

--- map.js
+++ map.js
@@ -7,5 +7,6 @@ var map = L.map('map').setView(center, 11);
 // Set up the OSM layer
 L.tileLayer(
   'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
     maxZoom: 18
   }).addTo(map);
