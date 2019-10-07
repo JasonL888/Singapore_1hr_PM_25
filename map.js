5a5b6213bc36f4b4077545078c82988691547e95
Jason Lau <jason.lau@axiros.com>
2019-10-07 16:17:49 +0800

    first drop with leaflet.js

--- map.js
+++ map.js
@@ -10,3 +10,10 @@ L.tileLayer(
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
     maxZoom: 18
   }).addTo(map);
+
+
+L.marker({lat:1.35735,lon:103.7},{title:"West"}).addTo(map).bindPopup("West").openPopup();
+L.marker({lat:1.35735,lon:103.94},{title:"East"}).addTo(map).bindPopup("East").openPopup();
+L.marker({lat:1.295875,lon:103.82},{title:"South"}).addTo(map).bindPopup("South").openPopup();
+L.marker({lat:1.41803,lon:103.82},{title:"North"}).addTo(map).bindPopup("North").openPopup();
+L.marker({lat:1.35735,lon:103.82},{title:"Central"}).addTo(map).bindPopup("Central").openPopup();
